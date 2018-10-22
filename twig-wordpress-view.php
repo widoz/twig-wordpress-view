ee750b7f4308ba8a05d698977b328ad733753536
Guido Scialfa <dev@guidoscialfa.com>
2018-10-22 21:47:27 +0200

    Introduce IIFE to prevent third parties to access to inner functions

--- twig-wordpress-view.php
+++ twig-wordpress-view.php
@@ -26,43 +26,47 @@ namespace TwigWordPressView;
 
 // phpcs:disable
 
-/**
- * Custom Admin Notice Message
- *
- * @param string $message
- * @param string $noticeType
- * @param array $allowedMarkup
- */
-function adminNotice(string $message, string $noticeType, array $allowedMarkup = []): void
-{
-    add_action('admin_notices', function () use ($message, $noticeType, $allowedMarkup) {
-        ?>
-        <div class="notice notice-<?= esc_attr($noticeType) ?>">
-            <p><?= wp_kses($message, $allowedMarkup) ?></p>
-        </div>
-        <?php
-    });
-}
-
-function bootstrap(): void
-{
-    $autoloader = plugin_dir_path(__FILE__) . '/vendor/autoload.php';
-
-    if (!file_exists($autoloader)) {
-        adminNotice(
-            sprintf(
-            // translators: %s Is the name of the plugin.
-                __('%s: No autoloader found, plugin cannot load properly.', 'twig-wordpress-view'),
-                '<strong>' . esc_html__('Twig WordPress View', 'twig-wordpress-view') . '</strong>'
-            ),
-            'error',
-            ['strong' => true]
-        );
-
-        return;
+(function () {
+    /**
+     * Custom Admin Notice Message
+     *
+     * @param string $message
+     * @param string $noticeType
+     * @param array $allowedMarkup
+     */
+    function adminNotice(string $message, string $noticeType, array $allowedMarkup = []): void
+    {
+        add_action('admin_notices', function () use ($message, $noticeType, $allowedMarkup) {
+            ?>
+            <div class="notice notice-<?= esc_attr($noticeType) ?>">
+                <p><?= wp_kses($message, $allowedMarkup) ?></p>
+            </div>
+            <?php
+        });
     }
 
-    require_once $autoloader;
-}
+    function bootstrap(): void
+    {
+        $autoloader = plugin_dir_path(__FILE__) . '/vendor/autoload.php';
 
-add_action('plugins_loaded', __NAMESPACE__ . '\\bootstrap');
+        if (!file_exists($autoloader)) {
+            adminNotice(
+                sprintf(
+                // translators: %s Is the name of the plugin.
+                    __('%s: No autoloader found, plugin cannot load properly.',
+                        'twig-wordpress-view'),
+                    '<strong>' . esc_html__('Twig WordPress View',
+                        'twig-wordpress-view') . '</strong>'
+                ),
+                'error',
+                ['strong' => true]
+            );
+
+            return;
+        }
+
+        require_once $autoloader;
+    }
+
+    add_action('plugins_loaded', __NAMESPACE__ . '\\bootstrap');
+})();
